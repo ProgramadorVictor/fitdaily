8cc70b3fec78a6a5a9f9c641774189b86b147f67
ProgramadorVictor <victorochaandrade@gmail.com>
2024-10-05 11:21:00 -0300

    style: add menu com fundo vermelho

--- resources/views/components/menu/front/menu.blade.php
+++ resources/views/components/menu/front/menu.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-custom-black">
-    <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <div class="container-fluid justify-content-end">
+        <button class="navbar-toggler bg-custom-red" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
