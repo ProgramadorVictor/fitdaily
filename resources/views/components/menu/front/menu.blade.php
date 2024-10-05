824767bbbe69a40ebd4080b46a7571661fe8ff32
ProgramadorVictor <victorochaandrade@gmail.com>
2024-10-05 11:06:14 -0300

    fix: adicionando funcionalidade ao menu

--- resources/views/components/menu/front/menu.blade.php
+++ resources/views/components/menu/front/menu.blade.php
@@ -3,25 +3,25 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse">
+        <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto gap-4">
-                <li class="nav-item ">
-                    <a class="nav-link active fw-bolder text-white bg-custom-black btn-border" href="{{route('tela-principal')}}">Chat</a>
-                </li>
-                <li class="nav-item ">
-                    <a class="nav-link active fw-bolder text-white bg-custom-black btn-border" href="{{route('usuario.perfil')}}">Perfil</a>
+                <li class="nav-item">
+                    <a class="nav-link active fw-bolder text-white" href="{{route('tela-principal')}}">Chat</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link fw-bolder text-white bg-custom-black btn-border" href="{{route('financeiro-pagamento.planos')}}">Planos</a>
+                    <a class="nav-link active fw-bolder text-white" href="{{route('usuario.perfil')}}">Perfil</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link fw-bolder text-white bg-custom-black btn-border" href="{{route('financeiro-pagamento.extratos')}}">Extratos</a>
+                    <a class="nav-link fw-bolder text-white" href="{{route('financeiro-pagamento.planos')}}">Planos</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link fw-bolder text-white bg-custom-black btn-border" href="{{route('usuario.treinos')}}">Treinos</a>
+                    <a class="nav-link fw-bolder text-white" href="{{route('financeiro-pagamento.extratos')}}">Extratos</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link fw-bolder text-white bg-custom-black btn-border" href="{{route('login.logout')}}">Logout</a>
+                    <a class="nav-link fw-bolder text-white" href="{{route('usuario.treinos')}}">Treinos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fw-bolder text-white" href="{{route('login.logout')}}">Logout</a>
                 </li>
             </ul>
         </div>
